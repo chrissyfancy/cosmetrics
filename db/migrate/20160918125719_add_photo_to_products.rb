e7bef0cd0d0cea7a9d81df51cae3e1f62bdd8cf9
Christina Fancy <chrissyfancy@gmail.com>
2016-10-31 19:51:05 -0400

    git commit -m "updated migrations"

--- db/migrate/20160918125719_add_photo_to_products.rb
+++
@@ -1,5 +0,0 @@
-class AddPhotoToProducts < ActiveRecord::Migration[5.0]
-  def change
-    add_column :products, :product_photo, :string, null: false, default: "#{Rails.root}/db/migrate/placeholder.png"
-  end
-end
--- db/migrate/20160919140034_drop_products_table.rb
+++
@@ -1,5 +0,0 @@
-class DropProductsTable < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :products
-  end
-end
