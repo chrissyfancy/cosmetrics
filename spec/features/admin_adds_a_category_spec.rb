5025289e1af9c8b956800639e006af619707b099
Chrissy Fancy <chrissyfancy@gmail.com>
2017-07-20 15:26:54 -0400

    Update new product and category feature tests

--- spec/features/admin_adds_a_category_spec.rb
+++ spec/features/admin_adds_a_category_spec.rb
@@ -2,17 +2,28 @@ require 'rails_helper'
 
 feature "an authorized admin" do
   let!(:admin) { FactoryGirl.create(:user, role: 'admin') }
-  let!(:category) { FactoryGirl.create(:category) }
 
-  scenario 'successfully adds a new category' do
+  scenario 'successfully adds a new category when information is valid' do
     login_as(admin)
     visit new_category_path
 
-    fill_in 'Name', with: category.name
-    fill_in 'Description', with: category.description
+    fill_in 'Name', with: "Skin Care"
+    fill_in 'Description', with: ""
     click_on 'Submit Category'
 
     expect(page).to have_content('Successfully added category.')
-    expect(page).to have_content(category.name)
+    expect(page).to have_content("Skin Care")
+  end
+
+  scenario 'unsuccessfully adds a new category when information is not valid' do
+    login_as(admin)
+    visit new_category_path
+
+    fill_in 'Name', with: ''
+    fill_in 'Description', with: ''
+    click_on 'Submit Category'
+
+    expect(page).to have_content("1 error prohibited this category from being saved")
+    expect(page).to have_content("Name can't be blank")
   end
 end
--- spec/features/admin_adds_a_product_spec.rb
+++ spec/features/admin_adds_a_product_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-feature "admin adds a new product" do
+feature "an authorized admin" do
   let!(:user) { FactoryGirl.create(:user) }
   let!(:admin) { FactoryGirl.create(:user, role: 'admin') }
-  let!(:product) { FactoryGirl.create(:product) }
+  let!(:category) { FactoryGirl.create(:category) }
 
-  scenario 'add a product' do
+  scenario 'successfully adds a new product when data is valid' do
     login_as(admin)
     visit new_product_path
 
@@ -14,11 +14,31 @@ feature "admin adds a new product" do
     fill_in 'Description', with: "Dust on Benefit's iconic hoola Matte Bronzer across the chin, cheeks, and forehead for a healthy, natural-looking tan year-round that flatters any skintone. Complete with soft, natural-bristle cheek brush, it delivers pure matte color—without leaving behind shine or shimmer."
     fill_in 'Price', with: 29.00
     fill_in 'Size', with: 0.28
-    select 'Skin Care', from: 'Category'
+    select category.name, from: 'Category'
     fill_in 'Photo URL', with: "/Users/christinafancy/challenges/online-review-site/sephora_reviews/db/migrate/placeholder.png"
     click_on 'Submit Product'
 
     expect(page).to have_content('Successfully added product.')
-    expect(page).to have_content('Hoola Matte Bronzer')
+    expect(page).to have_content("Hoola Matte Bronzer")
+    expect(page).to have_content("BENEFIT COSMETICS")
   end
+
+  scenario 'unsuccessfully adds a new product when data is invalid' do
+    login_as(admin)
+    visit new_product_path
+
+    fill_in 'Name', with: ""
+    fill_in 'Brand', with: ""
+    fill_in 'Description', with: ""
+    fill_in 'Price', with: ""
+    fill_in 'Size', with: ""
+    select category.name, from: 'Category'
+    fill_in 'Photo URL', with: ""
+    click_on 'Submit Product'
+
+    expect(page).to have_content("2 errors prohibited this product from being saved:")
+    expect(page).to have_content("Name can't be blank")
+    expect(page).to have_content("Brand can't be blank")
+  end
+
 end
