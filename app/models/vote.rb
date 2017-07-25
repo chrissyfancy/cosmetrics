900ed84ef38c07e5c7fb724599a08ba001d80ed1
Chrissy Fancy <chrissyfancy@gmail.com>
2017-07-25 11:42:49 -0400

    Update model validations

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,7 +4,7 @@ class Vote < ApplicationRecord
   validates :score, presence: true
   validates :score, numericality: { only_integer: true }
   validates :score, inclusion: { in: -1..1 }
-  validates :user, uniqueness: { scope: :review }
-  validates :user, presence: true
-  validates :review, presence: true
+  validates :user_id, uniqueness: { scope: :review }
+  validates :user_id, presence: true
+  validates :review_id, presence: true
 end
