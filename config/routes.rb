2609341b2d777a0a34c3fd306bf831b8c5270701
Chrissy Fancy <chrissyfancy@gmail.com>
2017-08-02 20:55:24 -0400

    Add review namespace under products

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :products
+      resources :products do
+        resources :reviews
+      end
     end
   end
 
